485405187712c4a5475fad19331f3992ce6d863d
chrissperb <chrissperb@gmail.com>
2024-04-22 19:35:11 -0300

    Reorganização das classes e pacotes de teste.

---
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -0,0 +1,29 @@
+package br.ce.wcaquino.servicos;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+
+public class LocacaoServiceTest {
+
+    @Test
+    public void teste() {
+        //cenario - criar e/ou instanciar todos os recursos necessários para o teste do método
+        LocacaoService service = new LocacaoService();
+        Usuario usuario = new Usuario("Usuario 1");
+        Filme filme = new Filme("Filme 1", 2, 5.0);
+
+        // ação - aplicar o método no ecnário criado
+        Locacao locacao = service.alugarFilme(usuario, filme);
+
+        //verificacao
+        Assertions.assertTrue(locacao.getValor() == 5.0);
+        Assertions.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+        Assertions.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+    }
+}
